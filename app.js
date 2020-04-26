2cf218e9e4da3753312d573a8f07098f1aa1a8be
Oscary Garcia <oscarygarcia.iban@gmail.com>
2020-04-26 16:21:47 -0400

    -Create connection from mongoDB -Create Mongodb model -Add data filter

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const chalk = require("chalk");
+const mongoose = require("mongoose");
 
 const app = express();
-
+const db = mongoose.connect("mongodb://localhost/bookAPI");
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require("./src/models/booksModel");
+
+bookRouter.route("/books").get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+
+  //   TODO:  const response = { hello: "This is a test!!" };
+  //   res.json(response);
+});
+
+bookRouter.route("/books/:bookId").get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
+});
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to nodemon my API!");
---
+++ booksJson.js
@@ -0,0 +1,26 @@
+db.books.insert([
+  {
+    title: "war and Peace",
+    genre: "Historical Fiction",
+    author: "lev Nikolayevich",
+    read: false,
+  },
+  {
+    title: "Deadpool",
+    genre: "sangrients",
+    author: "Bryan renol",
+    read: false,
+  },
+  {
+    title: "Watchmen",
+    genre: "Fiction",
+    author: "Dr. silvana",
+    read: false,
+  },
+  {
+    title: "Red dead redemption",
+    genre: "action",
+    author: "Recostar",
+    read: false,
+  },
+]);
