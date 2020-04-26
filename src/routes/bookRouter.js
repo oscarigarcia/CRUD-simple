f310c9b52571c2b83ce7dbb8135d3a51b619ca39
Oscary Garcia <oscarygarcia.iban@gmail.com>
2020-04-26 18:12:03 -0400

    Complete implemetation RestFul Add middleware

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const express = require("express");
 
 function routes(Book) {
@@ -9,7 +10,6 @@ function routes(Book) {
       const book = new Book(req.body);
 
       book.save();
-      console.log(book);
       res.status(201).json(book);
     })
     .get((req, res) => {
@@ -28,14 +28,65 @@ function routes(Book) {
       //   res.json(response);
     });
 
-  bookRouter.route("/books/:bookId").get((req, res) => {
+  // TODO: This is a middleware
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(book);
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(400);
     });
   });
+
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => {
+      res.json(req.book);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
+    });
 
   return bookRouter;
 }
